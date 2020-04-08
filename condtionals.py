90f98671678b281b870a4c34249d54918e250883
MAKMOT JOHNSON <makmotjohnson7@gmail.com>
2020-04-08 16:24:56 +0300

    Add files via upload

---
+++ condtionals.py
@@ -0,0 +1,48 @@
+# relating the virable similarity in memory
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(id(a))
+print(id(b))
+print(id(a) == id(b))
+print(id(a) is id(b))
+
+# conditonal if
+course = 'software'
+if course == 'software'
+print('The course is yours')
+elif
+print('It doesnt belong to you')
+else:
+    print('it is the real course you need')
+
+#  example 2
+
+condition = False
+if condition
+print('Evaluated to True')
+else
+print('Evaluated to False')
+condition = 10
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
+
+condition = None
+
+if condition:
+    print('Evaluated to True')
+    else:
+    print('Evaluated to False')
+
+# false values
+# False
+# none
+# zero of any numeric type
+# any empty sequence: for example, '',() , [].
+# any empty mapping. for example. {}
+
+except expression as identifier:
+    pass
